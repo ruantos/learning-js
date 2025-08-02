8eddea331ac62d8ed574b5b46303fc69aa40403a
Juan Oliveira <ruantos@proton.me>
2025-08-02 16:02:47 -0300

    feat: added checkResult function

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-// defining functionns
-function verificarChute(chute){
-	let num = 10; 
+// defining functions
+function getRandomNum(){
+	return parseInt(Math.random() * 10 + 1);
+}
+
+function verificarChute(){
+	let num = getRandomNum(); 
+	let chute = document.querySelector('input').value;
+
 
 	if (chute == num) {
 		return true;
 	}
+	return false;
+}
+
+function showResult(){
+	if (verificarChute() === true) {
+		alert('Você acertou!');
+	}
+	else{
+		alert('Você errou!');
+	}	
 }
 
 function showLove(){
 	alert('Eu amo js');
 }
 
+// f-strings on js
 function askCity(){
 	let city = prompt('Em que cidade você mora? ');
 	alert(`Eu estive em ${city} recentemente`);
@@ -22,3 +39,4 @@ title.innerHTML = "Jogo Secreto";
 
 let paragraph = document.querySelector('p');
 paragraph.innerHTML = "Escolha um número entre 0 e 10";
+
